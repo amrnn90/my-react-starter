83aafde5ce2fd8b8a681a368732039bb106b62a9
Amr Noman <amrnn90@gmail.com>
2020-03-09 23:07:28 +0300

    make it reusable from outside

---
+++ flight.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const scripts = require("my-react-starter");
+const command = process.argv[2];
+
+const options = {
+  /** source path */
+  srcPath: path.join(__dirname, "frontend"),
+
+  /** output path (Must be absolute) */
+  outputAbsPath: path.resolve(__dirname, "dist"),
+
+  /** name for js bundle */
+  jsBundleName: "bundle.js",
+
+  /** name for css bundle */
+  cssBundleName: "styles.css",
+
+  /** entry file (relative to srcPath) */
+  entry: "index.js",
+
+  /** index template used by HtmlWebPackPlugin (relative to srcPath) */
+  indexTemplate: "index.html",
+
+  /** path for files that should just get copied to output (relative to srcPath) */
+  staticPath: "static",
+
+  /** proxies: each item is an array of: [from, target] */
+  proxies: [],
+};
+
+switch (command) {
+  case "dev":
+    scripts.dev(options);
+    break;
+  case "build":
+    scripts.build(options);
+    break;
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const _dev = require("./scripts/dev");
+const _build = require("./scripts/build");
+
+const defaultOptions = {
+  /** source path */
+  srcPath: path.join(__dirname, "src"),
+
+  /** output path (Must be absolute) */
+  outputAbsPath: path.resolve(__dirname, "dist"),
+
+  /** name for js bundle */
+  jsBundleName: "bundle.js",
+
+  /** name for css bundle */
+  cssBundleName: "styles.css",
+
+  /** entry file (relative to srcPath) */
+  entry: "index.js",
+
+  /** index template used by HtmlWebPackPlugin (relative to srcPath) */
+  indexTemplate: "index.html",
+
+  /** path for files that should just get copied to output (relative to srcPath) */
+  staticPath: "static",
+
+  /** proxies: each item is an array of: [from, target] */
+  proxies: [],
+};
+
+const build = options => {
+  process.env.NODE_ENV = "production";
+
+  _build({ ...defaultOptions, ...options });
+};
+
+const dev = options => {
+  process.env.NODE_ENV = "development";
+
+  _dev({ ...defaultOptions, ...options });
+};
+
+module.exports = {
+  build,
+  dev,
+};
